3f75d8329191b2c40900e1144d914d11afc64fa3
tuannah <motchuchimnho@gmail.com>
2025-07-10 17:39:04 +0700

    create RegisterActivity

--- app/src/main/java/com/example/omegeleapp/MainActivity.java
+++ app/src/main/java/com/example/omegeleapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.omegeleapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_main); // Đây sẽ hiển thị layout bạn thiết kế
     }
 }
